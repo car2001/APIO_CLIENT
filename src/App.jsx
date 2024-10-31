eea9ab5e8b05144aa41b51dc0e74f96e0b05623c
car2001 <carlos.pingo@wedox.co>
2024-10-31 01:58:45 -0500

    Proyecto final

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { CssBaseline, Container, Typography, TextField, Button, AppBar, Toolbar, FormControl } from '@mui/material';
+import { CssBaseline, Container, Typography, TextField, Button, AppBar, Toolbar, FormControl, CircularProgress } from '@mui/material';
 import { WbSunny, NightsStay } from '@mui/icons-material';
+import axios from 'axios';
 
 const App = () => {
     const [darkMode, setDarkMode] = useState(true);
     const [imagePath, setImagePath] = useState('');
     const [imageFile, setImageFile] = useState(null);
     const [imageUrl, setImageUrl] = useState('');
+    const [responseImageUrl, setResponseImageUrl] = useState('');
+    const [responseMessage, setResponseMessage] = useState('');
+    const [canvasVisible, setCanvasVisible] = useState(false);
+    const [loading, setLoading] = useState(false); // State to manage loading
 
     const theme = createTheme({
         palette: {
@@ -27,19 +32,69 @@ const App = () => {
         if (file) {
             setImagePath(file.name);
             setImageFile(file);
-            setImageUrl(URL.createObjectURL(file)); // Crear URL para mostrar la imagen
+            setImageUrl(URL.createObjectURL(file));
+            setResponseImageUrl(''); // Clear previous response image
+            setCanvasVisible(false); // Hide canvas until the image is processed
         } else {
             setImagePath('');
             setImageUrl('');
         }
     };
 
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
         if (imageFile) {
-            console.log({ imageFile });
+            const base64String = await getBase64fromFile(imageFile);
+            const api_key = "L5zWZFeAMpeoIgt3j3AZ"; // Tu clave de API
+
+            setLoading(true); // Start loading
+
+            try {
+                const response = await axios({
+                    method: "POST",
+                    url: "https://detect.roboflow.com/yolov8-01/1",
+                    params: {
+                        api_key: api_key,
+                        format: "image",
+                        confidence: "40",
+                        overlap: "30",
+                        labels: "on",
+                        stroke: "2"
+                    },
+                    data: base64String,
+                    headers: {
+                        "responseType": 'image/jpeg',
+                    },
+                    responseType: 'arraybuffer',
+                });
+
+                const arrayBufferView = new Uint8Array(response.data);
+                const blob = new Blob([arrayBufferView], { type: 'image/jpeg' });
+                const base64image = window.URL.createObjectURL(blob);
+                setResponseImageUrl(base64image);
+                setCanvasVisible(true); // Show canvas after receiving response
+                setImageUrl(''); // Hide selected image
+            } catch (error) {
+                console.error('Error al enviar la imagen:', error);
+                setResponseMessage('Error al enviar la imagen');
+            } finally {
+                setLoading(false); // Stop loading
+            }
         } else {
             alert('Por favor, selecciona una imagen.');
         }
+    };
+
+    const getBase64fromFile = (file) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onloadend = () => {
+                resolve(reader.result);
+            };
+            reader.onerror = (error) => {
+                reject(error);
+            };
+        });
     };
 
     return (
@@ -47,7 +102,7 @@ const App = () => {
             <CssBaseline />
             <AppBar position="static" color="primary">
                 <Toolbar>
-                    <Typography variant="h6">Apio Detection</Typography> {/* Título actualizado */}
+                    <Typography variant="h6">Apio Detection</Typography>
                     <Button
                         color="inherit"
                         onClick={toggleDarkMode}
@@ -64,15 +119,15 @@ const App = () => {
                         label="Ruta de la imagen"
                         variant="outlined"
                         InputProps={{
-                            readOnly: true, // Hacer el input de solo lectura
+                            readOnly: true,
                         }}
-                        style={{ flexGrow: 1 }} // Para que el TextField ocupe el espacio restante
+                        style={{ flexGrow: 1 }}
                     />
                     <input
                         type="file"
                         accept="image/*"
                         onChange={handleImageChange}
-                        style={{ display: 'none' }} // Oculta el input file
+                        style={{ display: 'none' }}
                         id="file-upload"
                     />
                     <label htmlFor="file-upload">
@@ -82,18 +137,62 @@ const App = () => {
                     </label>
                 </FormControl>
 
-                <Button variant="contained" color="primary" onClick={handleSubmit}>
-                    Enviar
+                <Button variant="contained" color="primary" onClick={handleSubmit} disabled={loading}>
+                    {loading ? <CircularProgress size={24} /> : 'Enviar'}
                 </Button>
 
-                {/* Mostrar la imagen seleccionada */}
+                {/* Mostrar la imagen seleccionada antes de enviar */}
                 {imageUrl && (
-                    <div style={{ marginTop: '1rem', textAlign: 'center' }}>
+                    <div style={{ textAlign: 'center', marginTop: '1rem' }}>
                         <Typography variant="h6">Imagen seleccionada:</Typography>
-                        <img src={imageUrl} alt="Vista previa" style={{ maxWidth: '100%', maxHeight: '300px', marginTop: '0.5rem' }} />
+                        <img
+                            src={imageUrl}
+                            alt="Vista previa"
+                            style={{
+                                width: '100%',
+                                height: 'auto',
+                                objectFit: 'cover',
+                            }}
+                        />
                     </div>
                 )}
             </Container>
+
+            {/* Mostrar la imagen de respuesta en un canvas */}
+            {canvasVisible && responseImageUrl && (
+                <div style={{ margin: '2rem', textAlign: 'center' }}>
+                    <Typography variant="h6">Imagen de respuesta:</Typography>
+                    <canvas
+                        id="responseCanvas"
+                        style={{
+                            border: '1px solid #ccc',
+                            margin: '1rem',
+                            maxWidth: '100%', // Ajustar el tamaño máximo
+                            height: 'auto', // Mantener la relación de aspecto
+                        }}
+                    />
+                    <img
+                        src={responseImageUrl}
+                        alt="Respuesta"
+                        style={{ display: 'none' }} // Ocultar imagen, solo usar canvas
+                        onLoad={(e) => {
+                            const canvas = document.getElementById('responseCanvas');
+                            const context = canvas.getContext('2d');
+                            canvas.width = e.target.width;
+                            canvas.height = e.target.height;
+                            context.drawImage(e.target, 0, 0);
+                        }}
+                    />
+                </div>
+            )}
+
+            {/* Mostrar la respuesta del API */}
+            {responseMessage && (
+                <div style={{ marginTop: '20px' }}>
+                    <Typography variant="h6">Respuesta de Roboflow:</Typography>
+                    <pre>{JSON.stringify(responseMessage, null, 2)}</pre>
+                </div>
+            )}
         </ThemeProvider>
     );
 };
